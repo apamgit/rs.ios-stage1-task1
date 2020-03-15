43b6f8de7c16d59790a573eae86bbb27ffad7418
AlexParnitsky <europacorp@yandex.by>
2020-03-15 18:06:45 +0300

    feat: add solution to task 1-3

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,30 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    
+    int minValue = 0;
+    int maxValue = 0;
+    int sumValue = 0;
+    
+    for (NSNumber* number in array) {
+        sumValue += [number intValue];
+    }
+    
+    minValue = sumValue;
+    
+    for (NSNumber* number in array) {
+        int miniMaxSum = sumValue - [number intValue];
+        if (miniMaxSum > maxValue) {
+            maxValue = miniMaxSum;
+        }
+        if (miniMaxSum < minValue) {
+            minValue = miniMaxSum;
+        }
+    }
+    
+    return @[@(minValue), @(maxValue)];
 }
 
 @end
+
+
--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,18 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    
+    if (!bill || index < 0 || index >= [bill count]) {
+        return nil;
+    }
+    NSInteger currentSum = [[bill valueForKeyPath:@"@sum.self"] intValue];
+    currentSum -= [bill[index] intValue];
+    currentSum /= 2;
+    if ([sum intValue] == currentSum) {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat:@"%ld", [sum intValue] - currentSum];
+    }
 }
 
 @end
